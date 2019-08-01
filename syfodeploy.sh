e5900689113cc299432991a02dad937981b3f45c
Kevin Sillerud <kevin.sillerud@gmail.com>
2019-08-01 10:57:45 +0200

    Add the posibility to deploy the current git context

--- syfodeploy.sh
+++ syfodeploy.sh
@@ -24,12 +24,22 @@ fi
 circle_token=$(cat $config_file | grep "token:" | cut -b 8-)
 circle_host=$(cat $config_file | grep "host:" | cut -b 7-)
 
-if [ -z "$2" ]; then
-    echo "Usage $0 <repository> <branch>"
+project=$1
+branch=$2
+
+if [ "$project" == "git-context" ]; then
+    base_ref=$(basename `git config --get remote.origin.url`)
+    project="${base_ref%.*}"
+    branch=$(git rev-parse --abbrev-ref HEAD)
+fi
+
+if [ -z "$branch" ]; then
+    echo "Deploy a specific branch for a repository: $0 <repository> <branch>"
+    echo "Deploy using the current git context: $0 git-context"
     exit 1
 fi
 
-deploy_payload="{ \"branch\": \"$2\", \"parameters\": { \"deploy_branch\": true } }"
+deploy_payload="{ \"branch\": \"$branch\", \"parameters\": { \"deploy_branch\": true } }"
 
-curl -u "$circle_token:" -H "Content-Type: application/json" -d "$deploy_payload" "$circle_host/api/v2/project/gh/navikt/$1/pipeline"
+curl -u "$circle_token:" -H "Content-Type: application/json" -d "$deploy_payload" "$circle_host/api/v2/project/gh/navikt/$project/pipeline"
 
